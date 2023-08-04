8daad3ef86d506b4d014998accf787ba99d577a0
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2023-08-04 18:18:58 -0300

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+/// <reference types="vitest" />
+
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setupTests.ts',
+    css: true,
+    reporters: ['verbose'],
+  },
+});

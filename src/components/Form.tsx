d096d54eeaa9f206cfb95c4831820c4e3bde1c0d
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-08-23 11:55:12 -0300

    feat: requisito 04

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,51 @@
+import React, { useState } from 'react';
+
 type FormType = { handleClick: () => void };
 
+const INITIAL_DATA = {
+  name: '',
+  login: '',
+  password: '',
+  url: '',
+};
+
+function isPasswordValid(password: string) {
+  const passwordMin8andMax16 = password.length >= 8 && password.length <= 16;
+  const passwordHasLetAndNum = /[a-zA-Z]/.test(password) && /\d/.test(password);
+  const passwordHasCarac = /\W/.test(password);
+  const passwordValid = passwordMin8andMax16 && passwordHasLetAndNum && passwordHasCarac;
+  return passwordValid;
+}
+
 function Form({ handleClick }: FormType) {
+  const [inputsData, setInputsData] = useState(INITIAL_DATA);
+  const [passwordValid, setPasswordValid] = useState(false);
+  const [hasLogin, setHasLogin] = useState(false);
+  const [hasName, setHasName] = useState(false);
+  const [disabledBtn, setDisabledBtn] = useState(true);
+
+  const handleChange = (event:React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    if (name === 'password') {
+      const newPassword = value;
+      setPasswordValid(isPasswordValid(newPassword));
+    }
+    if (name === 'name') {
+      setHasName(value.length > 0);
+    }
+
+    if (name === 'login') {
+      setHasLogin(value.length > 0);
+    }
+    const formIsOk = hasLogin && hasName && passwordValid;
+    setDisabledBtn(!formIsOk);
+    setInputsData({
+      ...inputsData,
+      [name]: value,
+    });
+  };
+
   return (
     <form>
       <div>
@@ -8,6 +53,9 @@ function Form({ handleClick }: FormType) {
         <input
           type="text"
           id="name"
+          name="name"
+          value={ inputsData.name }
+          onChange={ handleChange }
         />
       </div>
       <div>
@@ -15,6 +63,9 @@ function Form({ handleClick }: FormType) {
         <input
           type="text"
           id="login"
+          name="login"
+          value={ inputsData.login }
+          onChange={ handleChange }
         />
       </div>
       <div>
@@ -22,6 +73,9 @@ function Form({ handleClick }: FormType) {
         <input
           type="password"
           id="password"
+          name="password"
+          value={ inputsData.password }
+          onChange={ handleChange }
         />
       </div>
       <div>
@@ -29,9 +83,12 @@ function Form({ handleClick }: FormType) {
         <input
           type="text"
           id="url"
+          name="url"
+          value={ inputsData.url }
+          onChange={ handleChange }
         />
       </div>
-      <button type="button">Cadastrar</button>
+      <button disabled={ disabledBtn } type="button">Cadastrar</button>
       <button type="button" onClick={ handleClick }>Cancelar</button>
     </form>
   );
